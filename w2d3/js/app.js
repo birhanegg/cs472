f4cecdc474854979c47fbd47db163dfa290a11bd
birhanegg <59857947+birhanegg@users.noreply.github.com>
2020-04-05 20:30:49 -0500

    mid review

--- w2d3/js/app.js
+++ w2d3/js/app.js
@@ -25,10 +25,15 @@ window.onload = function() {
 
     let john = Object.create(person);
     john.setName("John");
-    john.dateOfBirth = "1998-12-10";
+    john.dateOfBirth = new Date(1998, 12, 10);
 
     console.log(`\"The Persons name is  ${john.getName()}\"`);
     console.log(`\"${john.name} was born in  ${john.dateOfBirth}\"`);
+    let dob = john.name + " was born in";
+    dob += john.dateOfBirth.getFullYear() + "-";
+    dob += john.dateOfBirth.getFullYear() + "-"
+
+    console.log(dob);
 
 
     //---------------------Question-2 ---------------------------
@@ -36,7 +41,7 @@ window.onload = function() {
     let Employee = Object.create(person);
     Employee = {
         salary: 0,
-        hireDate: Date.now(),
+        hireDate: new Date(),
         doJob: function(jobTitle) {
             console.log(Employee.name + " is a " + jobTitle + " who earns $" + this.salary);
         }
@@ -47,6 +52,22 @@ window.onload = function() {
     Employee.doJob("Programmer");
 
 
+    let employee = {
+        salary: 0.0,
+        hireDate: new Date(),
+        //__proto__: person, //bad practice 
+        person,
+        doJob: function(title) {
+            console.log("========================================");
+            console.log("Inside Employe");
+            console.log("========================================");
+            console.log(`${this.name} is a ${title} who earns $ ${this.salary}`);
+        }
+    };
+    const anna = Object.create(employee);
+    anna.name = "Anna";
+    anna.salary = 249295.50;
+    anna.doJob("Programmer");
 
     //---------------------Question-3 ---------------------------
 
@@ -61,13 +82,13 @@ window.onload = function() {
     console.log(peter.toString());
 
 
-
     //---------------------Question-4 ---------------------------
 
     //Q4 solution shown below and also in github 
     //https://birhanegg.github.io/cs472/w1d4/index.html  //link
 
     //webform1
+    /*
     $("#webform1").submit(function(event) {
         console.log($("#email").val());
         console.log($("#password").val());
@@ -84,5 +105,5 @@ window.onload = function() {
         console.log($("#supplyDate").val());
         event.preventDefault();
     });
-
+*/
 };
