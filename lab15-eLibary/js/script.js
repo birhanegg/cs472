a9156a0d37837a22523b8a489b6566e0c586cf6b
birhanegg <59857947+birhanegg@users.noreply.github.com>
2020-04-12 21:28:42 -0500

    lab15 solution clean

--- lab15-eLibary/js/script.js
+++ lab15-eLibary/js/script.js
@@ -3,7 +3,7 @@
  * @since 2020-04-12
  */
 $(document).ready(function() {
-	
+
     //ajax get call 
     $.ajax({
         url: 'https://elibraryrestapi.herokuapp.com/elibrary/api/book/list',
@@ -46,45 +46,45 @@ $(document).ready(function() {
         const alertMessage = document.getElementById("alert");
         const alertMessage2 = document.getElementById("alertError");
 
-     e.preventDefault();
+        e.preventDefault();
 
-            const isbn = isbnTxt.value;
-            const title = titleTxt.value;
-            const overdue = parseFloat(overdueTxt.value);
-            const publisher = publisherTxt.value;
-            const datePublished = datePublishedTxt.value;
+        const isbn = isbnTxt.value;
+        const title = titleTxt.value;
+        const overdue = parseFloat(overdueTxt.value);
+        const publisher = publisherTxt.value;
+        const datePublished = datePublishedTxt.value;
 
-            const newBookData = {
-                "isbn": isbn,
-                "title": title,
-                "overdueFee": overdue,
-                "publisher": publisher,
-                "datePublished": datePublished
-            };
+        const newBookData = {
+            "isbn": isbn,
+            "title": title,
+            "overdueFee": overdue,
+            "publisher": publisher,
+            "datePublished": datePublished
+        };
 
-            fetch("https://elibraryrestapi.herokuapp.com/elibrary/api/book/add", {
-                method: "post",
-                body: JSON.stringify(newBookData),
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            }).then(function(response) {
-                return response.json();
-            }).then(function(jsonResponseData) {
-                console.log(jsonResponseData);
-                alertMessage.style.display = "block";
-                //clean inputs
-                isbnTxt.value = "";
-                titleTxt.value = "";
-                overdueTxt.value = "0.00";
-                publisherTxt.value = "";
-                datePublishedTxt.value = "";
+        fetch("https://elibraryrestapi.herokuapp.com/elibrary/api/book/add", {
+            method: "post",
+            body: JSON.stringify(newBookData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(function(response) {
+            return response.json();
+        }).then(function(jsonResponseData) {
+            console.log(jsonResponseData);
+            alertMessage.style.display = "block";
+            //clean inputs
+            isbnTxt.value = "";
+            titleTxt.value = "";
+            overdueTxt.value = "0.00";
+            publisherTxt.value = "";
+            datePublishedTxt.value = "";
 
-                titleTxt.focus();
-            }).catch(function(error) {
-                console.error(error);
-                alertMessage2.style.display = "block";
-            })
+            titleTxt.focus();
+        }).catch(function(error) {
+            console.error(error);
+            alertMessage2.style.display = "block";
+        })
     });
     //
 });
